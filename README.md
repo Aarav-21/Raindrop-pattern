92185d16dfeb8a55409df06a5ea57a9294e03959
Aarav-21 <62755926+Aarav-21@users.noreply.github.com>
2020-04-21 10:17:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Raindrop-pattern
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var rain1,rain2,rain3,rain4,rain5,rain6,rain7,rain8,rain9,rain10,rain11,rain12,rain13,rain14,rain15,rain16;
+var rain17,rain18,rain19,rain20,rain21,rain22,rain23,rain24,rain25;
+function setup() {
+  background(400,400);
+ rain1 = createSprite(250,204,3,80);
+ rain2 = createSprite(120,70,4,60);
+ rain3 = createSprite(200,350,3,90);
+ rain4 = createSprite(390,238,2,50);
+ rain5 = createSprite(220,180,3,80);
+ rain6 = createSprite(50,70,3,70);
+ rain7 = createSprite(90,105,3,60);
+ rain8 = createSprite(85,310,4,100);
+ rain9 = createSprite(350,350,2,60);
+ rain10 = createSprite(190,220,3,90);
+ rain11 = createSprite(260,334,3,80);
+ rain12 = createSprite(340,257,2,70);
+ rain13 = createSprite(140,291,3,80);
+ rain14 = createSprite(96,270,3,80);
+ rain15 = createSprite(287,198,3,70);
+ rain16 = createSprite(139,385,3,80);
+ rain17 = createSprite(211,160,3,80);
+ rain18 = createSprite(20,230,3,90);
+ rain19 = createSprite(40,370,3,80);
+ rain20 = createSprite(60,260,3,90);
+ rain21 = createSprite(370,20,3,70);
+ rain22 = createSprite(349,90,3,90);
+ rain23 = createSprite(299,89,3,80);
+ rain24 = createSprite(237,34,3,90);
+ rain25 = createSprite(149,125,3,80);
+ //rain25 = createSprite(250,204,3,80);
+
+}
+
+function draw() {
+  background("blue"); 
+  
+ 
+
+  drawSprites();
+}
